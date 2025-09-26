a0b63c393b3538bcd8d9260b7f5791cbb723b6ef
Sida Liu <learner.sida.liu@gmail.com>
2025-09-26 08:08:27 +0800

    update

--- activate.sh
+++ activate.sh
@@ -1,1 +1,1 @@
-export PATH="$HOME/.platformio/penv/bin:/Users/star/.platformio/packages/toolchain-xtensa-esp-elf/bin/:$PATH"
+export PATH="$HOME/.platformio/penv/bin:$HOME/.platformio/packages/toolchain-xtensa-esp-elf/bin/:$PATH"
