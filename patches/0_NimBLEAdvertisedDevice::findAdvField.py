febb2ab1ceaffae502cacaf76f8b0e33f88be0ad
Sida Liu <learner.sida.liu@gmail.com>
2025-09-28 10:47:31 +0800

    patch

---
+++ patches/0_NimBLEAdvertisedDevice::findAdvField.py
@@ -0,0 +1,25 @@
+# Instructions:
+# if there is a file .pio/libdeps/dictionary/NimBLE-Arduino/src/NimBLEAdvertisedDevice.cpp
+# and there is not a file .pio/libdeps/dictionary/NimBLE-Arduino/src/NimBLEAdvertisedDevice.cpp.bak
+
+# copy .pio/libdeps/dictionary/NimBLE-Arduino/src/NimBLEAdvertisedDevice.cpp to .pio/libdeps/dictionary/NimBLE-Arduino/src/NimBLEAdvertisedDevice.cpp.bak
+
+# copy new_files/NimBLEAdvertisedDevice.cpp to .pio/libdeps/dictionary/NimBLE-Arduino/src/NimBLEAdvertisedDevice.cpp
+
+# show message patched.
+
+from os.path import join, isfile
+import os
+import shutil
+
+Import("env")
+
+project_root = env.subst("$PROJECT_DIR")
+src_file = join(project_root, ".pio", "libdeps", "dictionary", "NimBLE-Arduino", "src", "NimBLEAdvertisedDevice.cpp")
+backup_file = src_file + ".bak"
+patched_file = join(project_root, "patches", "new_files", "NimBLEAdvertisedDevice.cpp")
+
+if isfile(backup_file): print("✓ Already patched"); exit(0)
+shutil.copy2(src_file, backup_file)
+shutil.copy2(patched_file, src_file)
+print("[Patch] 0_NimBLEAdvertisedDevice::findAdvField applied!")
